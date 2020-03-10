a7e091b6146d3ecd76fcca3707f000409aa8b715
jascha <jascha@jaeh.at>
2020-03-10 02:16:57 +0100

    Hero: header image now changes color

--- src/assets/modules/Hero.mjs
+++ src/assets/modules/Hero.mjs
@@ -1,10 +1,16 @@
-export const View = () =>
-  Link({ class: 'Hero', to: '/' }, Img({ height: 300, src: '/img/noncon2020-banner-light.jpg' }))
+export const View = ({ state }) =>
+  Link({ class: 'Hero', to: '/' }, Img({ height: 300, src: `/img/noncon2020-banner-${state.theme || 'light'}.jpg` }))
 
-export const style = vars => ({
+export const style = {
   width: '100%',
   display: 'block',
   padding: '1.5em 0 0',
+  margin: '0 auto',
+  backgroundColor: 'transparent',
+
+  '.light &&': {
+    backgroundColor: 'transparent',
+  },
 
   img: {
     display: 'block',
@@ -13,4 +19,4 @@ export const style = vars => ({
     maxWidth: '70vw',
     width: 'auto',
   },
-})
+}
